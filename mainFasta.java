29ad51854e4ce77e396b3b466647cb703b953403
JuanCa11 <juanca-once@hotmail.com>
2016-08-10 06:04:22 -0500

    Add files via upload

---
+++ mainFasta.java
@@ -0,0 +1,30 @@
+package Taller1;
+import java.util.*;
+import java.io.*;
+public class mainFasta {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String linea="";
+		StringBuilder sb = null;
+		StringBuilder salida = new StringBuilder();
+		while( (linea=bf.readLine())!=null && !linea.equals("")){
+			if(linea.charAt(0)=='>'){
+				if(sb!=null)
+					salida.append(new Fasta(sb).getCadena());
+				sb = new StringBuilder();
+			}
+			sb.append(linea);
+			sb.append("\n");
+		}
+		if(sb!=null){
+			salida.append(new Fasta(sb).getCadena());
+			System.out.print(salida.toString());
+		}
+	}
+
+}
